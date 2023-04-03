ba514d622a49a243cf95ae0aba43ab854c128d09
Rakhmatjon Kholmurotov <trecvord@gmail.com>
2023-04-03 20:13:55 +0300

    added some js files from my practice

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "clicked";  
+}
+
+document.getElementById("plus").innerHTML = 5 + 4;
+
+function myFunction2() {
+    document.getElementById("hello").innerHTML = "hello buddy!";
+    
+    document.getElementById("hello1").innerHTML = "how are you doing?";
+}
+
+function myFunction3() {
+  document.getElementById("demo2").innerHTML = "now you are krasav4eg!"
+}
---
+++ js/main2.js
@@ -0,0 +1,9 @@
+function myFunction1() {
+    window.alert("now you see me");
+}
+
+let x, y, z;
+x = 7;
+y = 2;
+z = x + y;
+document.getElementById("statement").innerHTML = "the value of z is " + z + ".";
